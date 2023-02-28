c4329bcc0d5c6ded9ee2a993d3d779b97b552239
Oleksandr Fedchik <fed4iksasha@gmail.com>
2023-02-28 12:40:56 +0200

    add junit

--- src/test/java/com/fedsav/homeaccountance/service/PurchaseItemServiceImplTest.java
+++ src/test/java/com/fedsav/homeaccountance/service/PurchaseItemServiceImplTest.java
@@ -3,14 +3,22 @@ package com.fedsav.homeaccountance.service;
 import com.fedsav.homeaccountance.model.dto.PurchaseItemDto;
 import com.fedsav.homeaccountance.model.entity.PurchaseItemEntity;
 import com.fedsav.homeaccountance.repository.PurchaseItemRepository;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.rules.ExpectedException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,12 +47,50 @@ class PurchaseItemServiceImplTest {
                 .name(testEntityName)
                 .build();
 
-        //when
         when(repository.save(purchaseItemEntity)).thenReturn(purchaseItemSavedEntity);
+        PurchaseItemDto purchaseItemDto = PurchaseItemDto.ofEntity(purchaseItemEntity);
+
+        //when
+
+        String purchaseItem = purchaseItemServiceImpl.createPurchaseItem(purchaseItemDto);
 
         //then
-        assertThat(purchaseItemServiceImpl.createPurchaseItem(PurchaseItemDto.ofEntity(purchaseItemEntity)),
-                equalTo(TEST_ID));
+        assertThat(purchaseItem, equalTo(TEST_ID));
+    }
+
+    @Test
+    void getPurchaseItem(){
+
+        //given
+        final String TEST_ID = "123456";
+        final String testEntityName = "testEntity";
+
+
+        PurchaseItemEntity purchaseItemReturnedEntity = PurchaseItemEntity.builder()
+                .id(TEST_ID)
+                .name(testEntityName)
+                .build();
+
+        when(repository.findById(TEST_ID)).thenReturn(Optional.ofNullable(purchaseItemReturnedEntity));
+        PurchaseItemDto purchaseItemDto = PurchaseItemDto.ofEntity(purchaseItemReturnedEntity);
+
+        PurchaseItemDto purchaseItemDtoService = purchaseItemServiceImpl.getPurchaseItem(TEST_ID);
+        //then
+        assertThat(purchaseItemDtoService, equalTo(purchaseItemDto));
+    }
+
+    @Test
+    void getPurchaseItemWithThrowException(){
+
+        final String TEST_ID = "123456";
+        String expectedMassage = "Purchase Item with id was not found: " + TEST_ID;
+
+        EmptyResultDataAccessException emptyResultDataAccessException = assertThrows(EmptyResultDataAccessException.class, () -> {
+            purchaseItemServiceImpl.getPurchaseItem(TEST_ID);
+        });
+
+        assertTrue(emptyResultDataAccessException.getMessage().contains(expectedMassage));
+
     }
 
 }
